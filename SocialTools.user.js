eaaaa07321152106f65c5a4efec2e690eefed1b7
Previously itsatacoshop247 <itsatacoshop247@gmail.com>
2016-04-21 00:59:32 -0700

    messaging update
    - fix some whitespace and missing ;
    - add message count in mass messaging

--- SocialTools.user.js
+++ SocialTools.user.js
@@ -59,26 +59,26 @@ $("header").append ( ' \
 
 //the whole process for doing the kick/ban and message has to be split up function wise to allow for the pages to load. There might be a way to trigger of a callback instead but this works for now.
 function sendMessage(nameStr, messageStr, kickOrBan) {
-    
+
     $("a[title='Message']").click(); //message button
     setTimeout(writeMessage, 1500, nameStr, messageStr, kickOrBan);
 }
 
 function unfriendMore() {
-    
+
     $("a[title='More Options']").click();
     setTimeout(unfriendUnfriend, 500);
 }
 
 function unfriendUnfriend() {
-    
+
     $("a[title='Unfriend']").click();
     setTimeout(unfriendConfirm, 1000);
 }
 
 function unfriendConfirm()
 {
-   $("a:contains('confirm')").click(); 
+   $("a:contains('confirm')").click();
    setTimeout(wipeBlack, 1000);
 }
 
@@ -88,40 +88,40 @@ function wipeBlack()
 }
 
 function writeMessage(nameStr, messageStr, kickOrBan) {
-    
+
     $("textarea[name='newMessageTextarea']").text(messageStr); //set message text
     $("a[id='btnSend']").click(); //send
-    
+
     if (kickOrBan === "kick")
     {
-       setTimeout(performKick, 1000, nameStr); 
+       setTimeout(performKick, 1000, nameStr);
     }
     else
     {
        setTimeout(performBan, 1000, nameStr);
     }
-    
+
 }
 
 function performKick(nameStr) {
     $("button:contains('kick')").click();
-    
+
     $("#infoText").text (nameStr + " has been kicked.");
     setTimeout(confirmAction, 1500);
 }
 
 function performBan(nameStr) {
-    
+
     $("button:contains('ban')").click();
-    
+
     $("#infoText").text (nameStr + " has been banned.");
     setTimeout(confirmAction, 1500);
-    
+
 }
 
 function confirmAction()
 {
-   $("a:contains('yes')").click(); 
+   $("a:contains('yes')").click();
    setTimeout(wipeBlack, 1000);
 }
 
@@ -132,21 +132,21 @@ function beginKickBan(kickOrBan)
     if (nameStr === "")
     {
         $("#infoText").text ("Enter a valid name.");
-        return
+        return;
     }
 
     if ($("label[for='check_" + nameStr + "']").length) //This is nice - You can actually force the click without having to hit 'Next' to search each page individually! We also look for the checkmark here to be sure we actually have permission to kick/ban them.
     {
-        $("label[for='check_" + nameStr + "']").click() //Checkmate!
+        $("label[for='check_" + nameStr + "']").click(); //Checkmate!
         if (!(messageStr === "Optional Message") && !(messageStr === ""))
         {
-            $("i[data-nickname='" + nameStr + "']").click() //The little dots on the player card
-            
+            $("i[data-nickname='" + nameStr + "']").click(); //The little dots on the player card
+
             setTimeout(sendMessage, 1500, nameStr, messageStr, kickOrBan);
         }
         else
         {
-            setTimeout(performKick, 1000, nameStr);   
+            setTimeout(performKick, 1000, nameStr);
         }
     }
     else
@@ -160,19 +160,19 @@ function toggleCheckMark(nameStr, state)
     if (nameStr === "")
     {
         $("#infoText").text ("Enter a valid name.");
-        return
+        return;
     }
 
     if ($("label[for='check_" + nameStr + "']").length) //This is nice - You can actually force the click without having to hit 'Next' to search each page individually! We also look for the checkmark here to be sure we actually have permission to kick/ban them.
     {
-        $("label[for='check_" + nameStr + "']").click() //Checkmate!
+        $("label[for='check_" + nameStr + "']").click(); //Checkmate!
         if (state === "check")
         {
             $("#infoText").text ("'" + nameStr + "' has been checkmarked. Use the original SocialClub Kick/Ban/Promote/Demote for this.");
         }
         else
         {
-            $("#infoText").text ("'" + nameStr + "' has been uncheckmarked.");   
+            $("#infoText").text ("'" + nameStr + "' has been uncheckmarked.");
         }
     }
     else
@@ -182,7 +182,7 @@ function toggleCheckMark(nameStr, state)
 }
 
 //--- Use jQuery to activate the dialog buttons.
-$("#kickButton").click ( function () {   
+$("#kickButton").click ( function () {
     beginKickBan("kick");
 } );
 
@@ -191,7 +191,7 @@ $("#CloseDlgBtn").click ( function () {
 } );
 
 $("#banButton").click ( function () {
-    beginKickBan("ban");  
+    beginKickBan("ban");
 } );
 
 $("#messageAll").click ( function () {
@@ -203,44 +203,48 @@ $("#messageAll").click ( function () {
 } );
 
 function messageFromListClickDots(players) {
-    
+
     if (players.length < 1)
     {
         $("#infoText").text ("Done with mass messaging.");
-        return
+        return;
     }
-    
+    else
+    {
+        $("#infoText").text ("" + players.length + " messages left");
+    }
+
     var messageStr = document.getElementById("message").value;
     if (!(messageStr === "Optional Message") && !(messageStr === ""))
-    {   
+    {
         players[0].click();
-        setTimeout(messageFromListClickMessage, 5000, players); 
+        setTimeout(messageFromListClickMessage, 5000, players);
     }
     else
     {
         $("#infoText").text ("Enter a message.");
-        return
+        return;
     }
 }
 
 function messageFromListClickMessage(players) {
-    
+
     $("a[title='Message']").click(); //message button
     setTimeout(messageFromListWriteMessage, 5000, players);
 }
 
 function messageFromListWriteMessage(players) {
-    
+
     $("textarea[name='newMessageTextarea']").text(document.getElementById("message").value); //set message text
     $("a[id='btnSend']").click(); //send
-    players.shift()
-    setTimeout(messageFromListClickDots, 5000, players);  
+    players.shift();
+    setTimeout(messageFromListClickDots, 5000, players);
 }
 
 function addDeleteButtons(){
 
     $("i[class='scicon-menu-dots player-card-actions']").each(function() {
-        
+
         if ($(this).find(".removeFriendClass").length < 1) //dont do anything if the player card already has the Delete Friend button
         {
             $(this).append('<br/><button id="removeFriend'+$(this).attr("data-nickname")+'" type="button" class="removeFriendClass"><h6>Delete Friend</h6></button>');
@@ -254,40 +258,40 @@ function addDeleteButtons(){
     });
 }
 
-$("#showRemover").click ( function () {   
+$("#showRemover").click ( function () {
     setInterval(addDeleteButtons, 250); //On the friends page, not all of the player cards load right away, so we'll just
 } );
 
 $("#playerPopup").click ( function () {
-    
+
     var nameStr = document.getElementById("name").value;
     if (nameStr === "")
     {
         $("#infoText").text ("Enter a valid name.");
-        return
+        return;
     }
 
     if ($("i[data-nickname='" + nameStr + "']").length) //we check for the dots here unlike the check for the checkmark in beginKickBan so that it works for anyone.
     {
-        $("i[data-nickname='" + nameStr + "']").click() //The little dots on the player card
+        $("i[data-nickname='" + nameStr + "']").click(); //The little dots on the player card
     }
     else
     {
         $("#infoText").text ("'" + nameStr + "' was not found.");
     }
-    
+
 } );
 
-$("#listByActive").click ( function () {   
+$("#listByActive").click ( function () {
     var players = new Array();
     $("a[class='player-card-actions']").each(function() {
         players.push({name: $(this).attr("data-nickname"), val: $(this).parent().next().children().children().first().html()}); //make an object to keep the name+crew linked and sortable.
-        
+
     });
-    
+
     var crewName =  $("#hierarchyFilterWrapper div[class='crewTag private']").children().first().html();
     $("header").append ( '<div id="PlayerList" style="text-align:center"> ');
-    
+
     for (var i = 0; i < players.length; i++)
 	{
         //name, active crew, and input for the checkbox. Each checkbox is linked to the player for tracking purposes.
@@ -297,9 +301,9 @@ $("#listByActive").click ( function () {
         }
     }
     $("header").append ( '</div>');
-    
+
     $("[id^=listBox]").click ( function () {  //This looks for any element starting with listBox, so listBoxUserA and listBox420yoloswagxXx are both hit.
-        if ( this.checked ) {   
+        if ( this.checked ) {
             toggleCheckMark(this.value, "check");
         } else {
             toggleCheckMark(this.value, "unchecked");
@@ -309,7 +313,7 @@ $("#listByActive").click ( function () {
 
 
 
-$("#listByRank").click ( function () {   
+$("#listByRank").click ( function () {
     var rank1 = new Array();
     var rank2 = new Array();
     var rank3 = new Array();
@@ -326,9 +330,9 @@ $("#listByRank").click ( function () {
     $("#crewRankWrapper_4 a[class='player-card-actions']").each(function() {
         rank4.push({name: $(this).attr("data-nickname"), val: new Date($(this).parent().next().html().replace('Joined: ',''))});
     });
-    
+
     //Skip sorting to keep alhpabetical
-    
+
     $("header").append ( '<div id="PlayerList" style="text-align:center"> ');
     $("header").append ( '<hr/ id="playerDate"><h1 id="playerDate" style="color:white;text-align:center">Rank 1 <input id="selectAll" type="checkbox" value="Rank 1"></h1><br/ id="playerDate">'); //Rank 1 + the checkmark for selecting all
     for (var i = 0; i < rank1.length; i++)
@@ -352,20 +356,20 @@ $("#listByRank").click ( function () {
 	}
     $("header").append ( '</div>');
     $("#infoText").text ("Rank List generated.");
-    
+
     $("[id^=listBox]").click ( function () {  //indivudual player checkboxes again
-        if ( this.checked ) {   
+        if ( this.checked ) {
             toggleCheckMark(this.value, "check");
         } else {
             toggleCheckMark(this.value, "unchecked");
         }
     });
-    
+
     $("[id^=selectAll]").click ( function () {  //the checkboxes for selecting all players in a rank
-       
+
        var rankToUse = this.value.replace('Rank ',''); //convert the value to just the number
-        
-        if ( this.checked ) {   
+
+        if ( this.checked ) {
             $("#crewRankWrapper_"+rankToUse+" a[class='player-card-actions']").each(function() { //go through each player and checkem
                 toggleCheckMark($(this).attr("data-nickname"), "check");
             });
@@ -377,9 +381,9 @@ $("#listByRank").click ( function () {
             $("#infoText").text ("All of " + this.value + " has been unchecked.");
         }
     });
-    
+
 } );
-    
+
 $("#CloseListBtn").click ( function () {
     $("*#playerDate").remove(); //This ends up removing every listing (*) related to either list based off playerDate being in the ID for each.
     $("#infoText").text ("List hidden.");
